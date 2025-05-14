40ab52d889ad476ac9d5a5e545093e0fb35a7c25
rafathossain <rafathossain96@gmail.com>
2025-05-14 14:43:43 +0600

    Haproxy

--- haproxy/deploy.sh
+++ haproxy/deploy.sh
@@ -7,7 +7,7 @@ mkdir -p /home/haproxy/certs
 mkdir -p /home/redis
 
 if [ ! -f .env ]; then
-    cp .env.example .env
+    cp .env.example /home/redis/.env
 fi
 
 if [ ! -f /home/haproxy/haproxy.cfg ]; then
@@ -16,6 +16,6 @@ fi
 
 docker-compose up -d
 
-echo "Updating .env file with REDIS_PASSWORD"
-echo "Update HAProxy Config in /home/haproxy/haproxy.cfg and restart container to apply changes."
-echo "Set the SSL Certificate in /home/haproxy/certs/uparzone.com.pem. The crt option should point to a combined certificate file that includes both the certificate and the private key in a single .pem file."
+printf "Update .env file (/home/redis/.env) with REDIS_PASSWORD\n\n"
+printf "Update HAProxy Config in /home/haproxy/haproxy.cfg and restart container to apply changes."
+printf "Set the SSL Certificate in /home/haproxy/certs/uparzone.com.pem. The crt option should point to a combined certificate file that includes both the certificate and the private key in a single .pem file."
