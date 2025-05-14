08547813326c538aa81a8b5660782a1323cf5885
rafathossain <rafathossain96@gmail.com>
2025-05-14 14:15:14 +0600

    Haproxy

--- haproxy/deploy.sh
+++ haproxy/deploy.sh
@@ -4,4 +4,10 @@ mkdir -p /home/haproxy
 mkdir -p /home/haproxy/certs
 mkdir -p /home/redis
 
+cp .env.example .env
+
 docker-compose up -d
+
+echo "Updating .env file with REDIS_PASSWORD"
+echo "Set the SSL Certificate in /home/haproxy/certs/uparzone.com.pem"
+echo "Set the SSL Certificate Key in /home/haproxy/certs/uparzone.com.pem.key"
